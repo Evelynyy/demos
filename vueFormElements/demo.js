08140317577b124b31fbef07f63b42c5b44e2aae
xiaolb <xiaoluobo.wang@foxmail.com>
2017-10-27 17:55:20 +0800

    vue 表单输入绑定实例

---
+++ vueFormElements/demo.js
@@ -0,0 +1,14 @@
+var demo1 = new Vue({
+	el: '#demo-1',
+	data: {
+		email: '',
+		username: '',
+		password: '',
+		phone: '',
+		sex: '',
+		birth: '',
+		hobby: [],
+		area: '',
+		intro: ''
+	}
+})
